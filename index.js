1e2d8d940665b37f3717f09e0d86d716ceb2286e
chin98edwin <edwin_chin98@hotmail.com>
2019-04-22 15:25:51 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const QuickMenu = {
+
+    DEFAULT_PAUSE: "Press any key to continue",
+
+    prompt: () => {
+        // ...
+    },
+
+    Component: class Component {
+
+        constructor() {
+            // ...
+        }
+
+        render() {}
+        componentDidCatch() {}
+        componentDidMount() {}
+        componentWillMount() {}
+        componentWillLoop() {}
+        componentWillUnmount() {}
+        __launch__() {}
+
+    }
+
+}
+
+export default QuickMenu
